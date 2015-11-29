95a0d345b834a9789ae7c7b74d9884506b78f8f5
Expersso <expersso5@gmail.com>
2015-11-29 15:21:29 +0100

    Add realistic example and disclaimer, add support for different download purposes.

--- R/gesis.R
+++ R/gesis.R
@@ -4,6 +4,19 @@
 #' always be the first function run when using the \code{gesis} package. See
 #' Workflow section below for further explanation.
 #'
+#' @param download_dir The directory (relative to your working directory) to
+#'   which you will be downloading files from GESIS.
+#' @param file_mime The MIME type of the file(s) you will be downloading (see
+#'   details).
+#'
+#' @details Most GESIS datasets are .dta or .spss files, which are of MIME type
+#'   "application/octet-stream". However, there are stray files stored as e.g.
+#'   .zip, which are of a different type ("application/zip" in this case). If
+#'   you notice your browser opening a download dialog and waiting for your
+#'   manual input (clicking OK to download), then odds are the file is not a
+#'   .dta/.spss file. You will then need to re-run the setup with a different
+#'   \code{file_mime} argument.
+#'
 #' @section Workflow: The GESIS website (\url{http://www.gesis.org}) offers a
 #'   large repository of datasets, mostly on public opinion surveys. However, it
 #'   does not offer a standard API, which makes accessing these datasets in a
@@ -19,18 +32,14 @@
 #'   (\code{download_dataset})} \item{(optional) Manually closing the Selenium
 #'   server (remDr$close(); remDr$closeServer())} }
 #'
-#' @param download_dir The directory (relative to your working directory) to
-#'   which you will be downloading files from GESIS.
-#' @param file_mime The MIME type of the file(s) you will be downloading (see
-#'   details).
-#'
-#' @details Most GESIS datasets are .dta or .spss files, which are of MIME type
-#'   "application/octet-stream". However, there are stray files stored as e.g.
-#'   .zip, which are of a different type ("application/zip" in this case). If
-#'   you notice your browser opening a download dialog and waiting for your
-#'   manual input (clicking OK to download), then odds are the file is not a
-#'   .dta/.spss file. You will then need to re-run the setup with a different
-#'   \code{file_mime} argument.
+#'   The \code{setup_gesis} function currently only supports Firefox. So if you
+#'   want to use a different browser, it is recommended that you look at the
+#'   code in that function, and the documentation in the \code{RSelenium}
+#'   (\href{https://cran.r-project.org/web/packages/RSelenium/vignettes/RSelenium-saucelabs.html})
+#'   package on how to set up a remote driver using other browsers. Once you
+#'   have successfully initiated a remote browser with a different browser, the
+#'   subsequent steps in the workflow should work as described in this
+#'   documentation.
 #'
 #' @return A Selenium remote driver.
 #'
@@ -96,8 +105,22 @@ login_gesis <- function(remDr,
 #' @param remDr Selenium remote driver created with \code{setup_gesis}.
 #' @param doi The unique identifier for the dataset to be downloaded (see details).
 #' @param filetype The filetype to be downloaded (usually only "dta" or "spss" available).
+#' @param purpose The purpose for which you are downloading the data set (see details).
+#' @param msg If TRUE, outputs a message showing which data set is being downloaded.
 #'
-#' @details Datasets reposited with GESIS are uniquely identified with a numberic identifier called a "DOI". This identifier appears both in the URL for a dataset's website, and on the website itself.
+#' @details Datasets reposited with GESIS are uniquely identified with a
+#'   numberic identifier called a "DOI". This identifier appears both in the URL
+#'   for a dataset's website, and on the website itself.
+#'
+#'   In addition to accepting the terms of use, you need to input a purpose for
+#'   downloading a data set. The options are as follows:
+#'
+#' 1. for scientific research (incl. PhD)
+#' 2. for reserach with commercial mandate
+#' 3. for teaching as lecturer
+#' 4. for my academic studies
+#' 5. for my final exam (e.g. bachelor or master)
+#' 6. for professional training and qualification
 #'
 #' @return Downloads a file.
 #'
@@ -105,36 +128,45 @@ login_gesis <- function(remDr,
 #' \dontrun{
 #' gesis_remDr <- setup_gesis(download_dir = "downloads")
 #' login_gesis(gesis_remDr, user = "myusername", pass = "mypassword")
-#' download_dataset(gesis_remDr, doi = "5928")
+#' download_dataset(gesis_remDr, doi = 5928)
 #' }
 #'
 #' @export
-download_dataset <- function(remDr, doi, filetype = "dta") {
+download_dataset <- function(remDr, doi, filetype = "dta", purpose = 1,
+                             msg = TRUE) {
 
-  url <- paste0("https://dbk.gesis.org/dbksearch/SDesc2.asp?ll=10&notabs=1&no=",
-                doi)
+    if(msg) message("Downloading DOI: ", doi, sprintf(" (%s)", Sys.time()))
 
-  remDr$navigate(url)
+    url <- paste0("https://dbk.gesis.org/dbksearch/SDesc2.asp?ll=10&notabs=1&no=",
+                  doi)
 
-  # Click filename to download .dta file
-  file_to_download <- sprintf("//a[contains(text(), '%s')]", filetype)
-  remDr$findElement("xpath", file_to_download)$clickElement()
+    purpose <- as.character(purpose)
 
-  # Input purpose and terms of use
-  remDr$switchToWindow(remDr$getWindowHandles()[[1]][2])
+    if(!purpose %in% as.character(1:6)) {
+        stop("Purpose has to be in the range 1-6 (see help).")
+    }
 
-  # Only check "accept terms of purpose" if unchecked
-  try(if(remDr$findElement("name",
-      "projectok")$getElementAttribute("checked")[[1]][1] != "true") {
-      remDr$findElement("name", "projectok")$clickElement()
-  }, silent = TRUE)
+    remDr$navigate(url)
 
-  remDr$findElement("xpath", "//option[@value='1']")$clickElement()
-  remDr$findElement("xpath", "//input[@value='Download']")$clickElement()
+    # Click filename to download .dta file
+    file_to_download <- sprintf("//a[contains(text(), '%s')]", filetype)
+    remDr$findElement("xpath", file_to_download)$clickElement()
 
-  # Close Download window and switch back to first window
-  remDr$closeWindow()
-  remDr$switchToWindow(remDr$getWindowHandles()[[1]])
+    # Input purpose and terms of use
+    remDr$switchToWindow(remDr$getWindowHandles()[[1]][2])
+
+    # Only check "accept terms of purpose" if unchecked
+    try(if(remDr$findElement("name",
+                             "projectok")$getElementAttribute("checked")[[1]][1] != "true") {
+        remDr$findElement("name", "projectok")$clickElement()
+    }, silent = TRUE)
+
+    remDr$findElement("xpath", sprintf("//option[@value='%s']", purpose))$clickElement()
+    remDr$findElement("xpath", "//input[@value='Download']")$clickElement()
+
+    # Close Download window and switch back to first window
+    remDr$closeWindow()
+    remDr$switchToWindow(remDr$getWindowHandles()[[1]])
 }
 
 #' Browse dataset codebook
@@ -157,19 +189,12 @@ browse_codebook <- function(doi, browseURL = TRUE, ...) {
            call. = FALSE)
   }
 
-  if (!requireNamespace("rvest", quietly = TRUE)) {
-      stop("rvest package needed for this function to work. Please install it.",
-           call. = FALSE)
-    }
-
   doi <- as.character(doi)
   base_url <- "https://dbk.gesis.org/dbksearch/"
   url <- paste0(base_url, "SDesc2.asp?ll=10&notabs=1&no=", doi)
   page <- xml2::read_html(url)
-  codebook_link <- rvest::html_nodes(page,
-                                     xpath = "//a[contains(text(), 'cdb.pdf')]")
-  codebook_link_href <- paste0(base_url,
-                               rvest::html_attr(codebook_link, "href"))
+  codebook_link <- xml2::xml_find_all(page, "//a[contains(text(), 'cdb.pdf')]")
+  codebook_link_href <- paste0(base_url, xml2::xml_attr(codebook_link, "href"))
 
   if(browseURL) {
     browseURL(codebook_link_href, ...)
