72764bea3988722b348601b45b4cc29bf6d3e9c3
Expersso <expersso5@gmail.com>
2016-08-16 13:17:52 +0200

    Remove dependence on Selenium

--- R/gesis.R
+++ R/gesis.R
@@ -1,110 +1,43 @@
-#' Prepare connection to GESIS
+#' Log in to the Gesis website
 #'
-#' Set up options necessary for automatically downloading files from GESIS. Will
-#' always be the first function run when using the \code{gesis} package. See
-#' Workflow section below for further explanation.
+#' @param username Your Gesis username
+#' @param password Your Gesis password
 #'
-#' @param download_dir The directory (relative to your working directory) to
-#'   which you will be downloading files from GESIS.
-#' @param file_mime The MIME type of the file(s) you will be downloading (see
-#'   details).
+#' @details
+#' The username and password can also be stored as enviroenment variables
+#' "GESIS_USER" and "GESIS_PASS" so as not to store these in plaintext in a
+#' script.
 #'
-#' @details Most GESIS datasets are .dta or .spss files, which are of MIME type
-#'   "application/octet-stream". However, there are stray files stored as e.g.
-#'   .zip, which are of a different type ("application/zip" in this case). If
-#'   you notice your browser opening a download dialog and waiting for your
-#'   manual input (clicking OK to download), then odds are the file is not a
-#'   .dta/.spss file. You will then need to re-run the setup with a different
-#'   \code{file_mime} argument.
-#'
-#' @section Workflow: The GESIS website (\url{http://www.gesis.org}) offers a
-#'   large repository of datasets, mostly on public opinion surveys. However, it
-#'   does not offer a standard API, which makes accessing these datasets in a
-#'   programmatic and reproducible way difficult. The \code{gesis} package gets
-#'   around this issue through the use of Selenium
-#'   (\url{http://www.seleniumhq.org/}) and the \code{RSelenium} package.
-#'   Selenium allows you to emulate a web browser session, wherein you log in to
-#'   the GESIS website, browse to the dataset of interest, click to download
-#'   that dataset, agree to accept the terms of use, and, ultimately, download
-#'   the dataset. This whole process follows three steps: \enumerate{
-#'   \item{Initiate a Selenium server (\code{setup_gesis})} \item{Log in to
-#'   GESIS (\code{gesis_login})} \item{Download a specified dataset
-#'   (\code{download_dataset})} \item{(optional) Manually closing the Selenium
-#'   server (remDr$close(); remDr$closeServer())} }
-#'
-#'   The \code{setup_gesis} function currently only supports Firefox. So if you
-#'   want to use a different browser, it is recommended that you look at the
-#'   code in that function, and the
-#'   \href{https://cran.r-project.org/web/packages/RSelenium/vignettes/RSelenium-saucelabs.html}{documentation}
-#'   in the \code{RSelenium} package on how to set up a remote driver using
-#'   other browsers. Once you have successfully initiated a remote browser with
-#'   a different browser, the subsequent steps in the workflow should work as
-#'   described in this documentation.
-#'
-#' @return A Selenium remote driver.
+#' @return A session object
+#' @export
+#' @importFrom rvest html_session, html_form, set_values, submit_form
 #'
 #' @examples
-#' \dontrun{
-#' gesis_remDr <- setup_gesis(download_dir = "downloads")
-#' login_gesis(gesis_remDr, user = "myusername", pass = "mypassword")
-#' download_dataset(gesis_remDr, doi = 5928)
-#' }
-#' @export
-setup_gesis <- function(download_dir = ".", file_mime = "application/octet-stream") {
+#' \dontrun{s <- login("my_gesis_username", "my_gesis_password")}
+login <- function(username = "", password = "") {
+    if(username == "") username <- Sys.getenv("GESIS_USER")
+    if(password == "") password <- Sys.getenv("GESIS_PASS")
 
-  # set firefox properties to not open download dialog
-  fprof <- RSelenium::makeFirefoxProfile(list(
-    browser.download.dir = paste0(getwd(), "/", download_dir),
-    browser.download.folderList = 2L,
-    browser.download.manager.showWhenStarting = FALSE,
-    browser.helperApps.neverAsk.saveToDisk = file_mime))
+    if(any(username == "", password == "")) {
+        stop("Please provide username and/or password.", call. = FALSE)
+    }
 
-  # Set up server as open initial window
-  RSelenium::checkForServer()
-  RSelenium::startServer()
-  remDr <- RSelenium::remoteDriver(extraCapabilities = fprof)
-  remDr$open()
-  return(remDr)
+    url <- "https://dbk.gesis.org/dbksearch/index.asp"
+    s <- html_session(url)
+    form <- html_form(s)[[1]]
+    form <- set_values(form, user = username, pass = password)
+    form$url <- ""
+    suppressMessages(submit_form(s, form))
 }
 
-#' Log in to GESIS
+#' Download a Gesis data set
 #'
-#' Create connection with GESIS and log in.
-#'
-#' @param remDr The remote driver object created with \code{setup_gesis}.
-#' @param user,pass Your GESIS user name and password.
-#'
-#' @return Nothing.
-#'
-#' @examples
-#' \dontrun{
-#' gesis_remDr <- setup_gesis(download_dir = "downloads")
-#' login_gesis(gesis_remDr, user = "myusername", pass = "mypassword")
-#' download_dataset(gesis_remDr, doi = 5928)
-#' }
-#'
-#' @export
-login_gesis <- function(remDr,
-                        user = getOption("gesis_user"),
-                        pass = getOption("gesis_pass")) {
-
-  remDr$navigate("https://dbk.gesis.org/dbksearch/gdesc.asp")
-  remDr$findElement(using = "id", value = "loginContainer")$clickElement()
-
-  remDr$findElement(using = "name", "user")$sendKeysToElement(list(user))
-  remDr$findElement(using = "name", "pass")$sendKeysToElement(list(pass))
-  remDr$findElement(using = "id", "login")$clickElement()
-}
-
-#' Download dataset from GESIS
-#'
-#' Download dataset from GESIS identified by its Document Object Identifier (DOI) and filetype
-#'
-#' @param remDr Selenium remote driver created with \code{setup_gesis}.
-#' @param doi The unique identifier for the dataset to be downloaded (see details).
-#' @param filetype The filetype to be downloaded (usually only "dta" or "spss" available).
-#' @param purpose The purpose for which you are downloading the data set (see details).
-#' @param msg If TRUE, outputs a message showing which data set is being downloaded.
+#' @param s A session object created with login()
+#' @param doi The unique identifier(s) for the data set(s)
+#' @param path Directory to which to download the file
+#' @param filetype The filetype to download (usually available: .dta/.por/.sav)
+#' @param purpose The purpose for downloading the data. See details.
+#' @param quiet Whether to output download message.
 #'
 #' @details Datasets reposited with GESIS are uniquely identified with a
 #'   numberic identifier called a "DOI". This identifier appears both in the URL
@@ -120,89 +53,114 @@ login_gesis <- function(remDr,
 #' 5. for my final exam (e.g. bachelor or master)
 #' 6. for professional training and qualification
 #'
-#' @return Downloads a file.
+#' @return
+#' @export Nothing
+#' @importFrom rvest jump_to, follow_link, html_form, set_values, submit_form
+#' @importFrom httr stop_for_status, content
 #'
 #' @examples
-#' \dontrun{
-#' gesis_remDr <- setup_gesis(download_dir = "downloads")
-#' login_gesis(gesis_remDr, user = "myusername", pass = "mypassword")
-#' download_dataset(gesis_remDr, doi = 5928)
-#' }
-#'
-#' @export
-download_dataset <- function(remDr, doi, filetype = "dta", purpose = 1, msg = TRUE) {
+#' \dontrun{s <- login("my_gesis_username", "my_gesis_password")
+#' download_dataset(s, doi = "0078")}
+download_dataset <- function(s, doi, path = ".", filetype = ".dta", purpose = 1,
+                             quiet = FALSE) {
+    for(d in doi) {
 
-    if(msg) message("Downloading DOI: ", doi, sprintf(" (%s)", Sys.time()))
+        url <- paste0("https://dbk.gesis.org/dbksearch/SDesc2.asp?db=E&no=", d)
+        s <- jump_to(s, url)
+        stop_for_status(s)
 
-    url <- paste0("https://dbk.gesis.org/dbksearch/SDesc2.asp?ll=10&notabs=1&no=",
-                  doi)
+        s <- follow_link(s, xpath = sprintf("//a[contains(text(), '%s')]", filetype))
+        stop_for_status(s)
 
-    purpose <- as.character(purpose)
+        form <- html_form(s)[[2]]
+        form <- set_values(form, zweck = 1, projectok = 1)
+        form$url <- ""
+        s <- submit_form(s, form)
+        stop_for_status(s)
 
-    if(!purpose %in% as.character(1:6)) {
-        stop("Purpose has to be in the range 1-6 (see help).")
+        if(!quiet) message("Downloading DOI: ", d)
+        filename <- gsub("^.*?\"|\"", "", s$response$headers$`content-disposition`)
+        filename <- file.path(path, filename)
+        writeBin(content(s$response, "raw"), filename)
     }
-
-    remDr$navigate(url)
-
-    # Click filename to download .dta file
-    file_to_download <- sprintf("//a[contains(text(), '%s')]", filetype)
-    remDr$findElement("xpath", file_to_download)$clickElement()
-
-    # Input purpose and terms of use
-    remDr$switchToWindow(remDr$getWindowHandles()[[1]][2])
-
-    # Only check "accept terms of purpose" if unchecked
-    try(if(remDr$findElement("name",
-                             "projectok")$getElementAttribute("checked")[[1]][1] != "true") {
-        remDr$findElement("name", "projectok")$clickElement()
-    }, silent = TRUE)
-
-    remDr$findElement("xpath", sprintf("//option[@value='%s']", purpose))$clickElement()
-    remDr$findElement("xpath", "//input[@value='Download']")$clickElement()
-
-    # Close Download window and switch back to first window
-    remDr$closeWindow()
-    remDr$switchToWindow(remDr$getWindowHandles()[[1]])
 }
 
-#' Browse dataset codebook
+#' Download the codebook for a Gesis data set
 #'
-#' Open a dataset's codebook in browser
+#' @param s A session object created with login()
+#' @param doi The unique identifier(s) for the data set(s)
+#' @param path Directory to which to download the file
+#' @param quiet Whether to output download message.
 #'
-#' @param doi The dataset's DOI.
-#' @param browseURL If FALSE, returns codebook's URL instead of opening in
-#'   browser.
-#' @param ... Additional arguments passed to \code{browseURL}.
+#' @return Nothing
+#' @export
+#' @importFrom rvest jump_to, follow_link
+#' @importFrom httr stop_for_status, content
 #'
 #' @examples
-#' \dontrun{browse_codebook("5928")}
+#' s <- login("my_gesis_username", "my_gesis_password")
+#' download_codebook(s, doi = "0078")
+download_codebook <- function(s, doi, path = ".", quiet = FALSE) {
+    for(d in doi) {
+
+        url <- paste0("https://dbk.gesis.org/dbksearch/SDesc2.asp?db=E&no=", d)
+        s <- jump_to(s, url)
+        stop_for_status(s)
+
+        s <- follow_link(s, xpath = "//a[contains(text(), '_cdb')]")
+        stop_for_status(s)
+
+        if(!quiet) message("Downloading codebook for DOI: ", d)
+        filename <- gsub("^.*?\"|\"", "", s$response$headers$`content-disposition`)
+        filename <- file.path(path, filename)
+        writeBin(content(s$response, "raw"), filename)
+    }
+}
+
+#' Get a dataframe with all available groups of studies
 #'
+#' @return A dataframe
 #' @export
-browse_codebook <- function(doi, browseURL = TRUE, ...) {
+#'
+#' @examples
+#' groups <- get_study_groups()
+#' head(groups)
+get_study_groups <- function() {
+    url <- "https://dbk.gesis.org/dbksearch/gdesc.asp"
+    page <- read_html(url)
 
-  if (!requireNamespace("xml2", quietly = TRUE)) {
-      stop("xml2 package needed for this function to work. Please install it.",
-           call. = FALSE)
-  }
+    group_no <- html_attr(html_nodes(page, xpath = "//input//parent::a//input"), "name")
+    group_no <- gsub("TI", "", group_no)
+    value <- html_attr(html_nodes(page, xpath = "//input//parent::a//input"), "value")
 
-  doi <- as.character(doi)
-  base_url <- "https://dbk.gesis.org/dbksearch/"
-  url <- paste0(base_url, "SDesc2.asp?ll=10&notabs=1&no=", doi)
-  page <- xml2::read_html(url)
-  codebook_link <- xml2::xml_find_all(page, "//a[contains(text(), 'cdb.pdf')]")
+    df <- data.frame(group_no, value)
+    class(df) <- c("tbl_df", "tbl", "data.frame")
+    df
+}
 
-  if(length(codebook_link) == 0) {
-      message("No codebook appears to be available. ",
-              "You might want to try to look for it manually:\n", url)
-      return(invisible())
-  }
+#' Get a dataframe of all individual data sets within a group of studies
+#'
+#' @param group_no
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @examples
+#' # Get DOIs and titles for all Eurobarometer studies
+#' eurobars <- get_datasets("0008")
+#' head(eurobars)
+get_datasets <- function(group_no) {
+    url <- paste0("https://dbk.gesis.org/dbksearch/GDESC2.asp?db=e&no=", group_no)
+    page <- read_html(url)
 
-  codebook_link_href <- paste0(base_url, xml2::xml_attr(codebook_link, "href"))
+    nodes <- html_nodes(page, xpath = "//li//a[contains(@href, 'no=')]")
+    text <- html_nodes(page, xpath = "//li//a[contains(@href, 'no=')]//parent::li")
+    text <- html_text(text, TRUE)
 
-  if(browseURL) {
-    browseURL(codebook_link_href, ...)
-  } else {
-    return(codebook_link_href)
-  }
+    doi <- substr(text, 3, 6)
+    title <- substr(text, 8, stop = 10000L)
+
+    df <- data.frame(doi, title)
+    class(df) <- c("tbl_df", "tbl", "data.frame")
+    df
 }
